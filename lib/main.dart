46d19dc1a2e34d8549019fc8e61d4809bed6684e
Allauddin Yousafzai <allauddin2015@gmail.com>
2020-05-09 17:56:34 +0500

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:secondbase/secondpage.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Home());
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        routes: <String, WidgetBuilder>{
+          '/secondpage': (BuildContext context) => new Secondpage(),
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyApp());
+  }
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -28,215 +45,214 @@ class _MyAppState extends State<MyApp> {
       'https://i.pinimg.com/originals/2e/2f/ac/2e2fac9d4a392456e511345021592dd2.jpg';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Project Name'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => Secondpage(),
+              ));
+            },
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            iconSize: 24,
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.add_call,
+              color: Colors.white,
+            ),
+            iconSize: 24,
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.local_bar,
+              color: Colors.white,
+            ),
+            iconSize: 24,
+          )
+        ],
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Project Name'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onpressed,
+        currentIndex: _currentindex,
+        iconSize: 40,
+        elevation: 9,
+        fixedColor: Colors.deepPurple,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+              activeIcon: Icon(Icons.camera)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.phone),
+              title: Text('Contact us'),
+              activeIcon: Icon(Icons.email)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.redAccent,
+        child: Icon(Icons.add),
+        onPressed: () => debugPrint('Button Clicked'),
+        splashColor: Colors.yellow,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: 400,
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage('https://images.unsplash'
+                            '.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+                        fit: BoxFit.cover)),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(picture),
+                ),
+                accountName: Text('Allauddin'),
+                accountEmail: Text('allauddin2015@gmail.com'),
               ),
-              iconSize: 24,
             ),
-            IconButton(
-              icon: Icon(
-                Icons.add_call,
-                color: Colors.white,
+            ListTile(
+              leading: Container(
+                padding: EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                child: Icon(
+                  Icons.home,
+                  size: 34,
+                  color: Colors.white,
+                ),
               ),
-              iconSize: 24,
+              title: Text(
+                'Home',
+              ),
+              subtitle: Text('Dashboard'),
             ),
-            IconButton(
-              icon: Icon(
-                Icons.local_bar,
-                color: Colors.white,
+            ListTile(
+              onTap: () {},
+              leading: Container(
+                padding: EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                child: Icon(
+                  Icons.home,
+                  size: 34,
+                  color: Colors.white,
+                ),
               ),
-              iconSize: 24,
+              title: Text(
+                'Second Page',
+              ),
+              subtitle: Text('Other Page'),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border(
+                bottom: BorderSide(
+                    color: Colors.redAccent,
+                    width: 1,
+                    style: BorderStyle.solid),
+              )),
+              child: ListTile(
+                leading: Container(
+                  padding: EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.red,
+                  ),
+                  child: Icon(
+                    Icons.home,
+                    size: 34,
+                    color: Colors.white,
+                  ),
+                ),
+                title: Text(
+                  'Home',
+                ),
+                subtitle: Text('Dashboard'),
+              ),
+            ),
+            ListTile(
+              leading: Container(
+                padding: EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                child: Icon(
+                  Icons.home,
+                  size: 34,
+                  color: Colors.white,
+                ),
+              ),
+              title: Text(
+                'Home',
+              ),
+              subtitle: Text('Dashboard'),
             )
           ],
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: onpressed,
-          currentIndex: _currentindex,
-          iconSize: 40,
-          elevation: 9,
-          fixedColor: Colors.deepPurple,
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text('Home'),
-                activeIcon: Icon(Icons.camera)),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.phone),
-                title: Text('Contact us'),
-                activeIcon: Icon(Icons.email)),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.redAccent,
-          child: Icon(Icons.add),
-          onPressed: () => debugPrint('Button Clicked'),
-          splashColor: Colors.yellow,
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        drawer: Drawer(
-          child: ListView(
+      ),
+      body: Container(
+          width: double.infinity,
+          color: Colors.deepPurpleAccent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
-                height: 400,
-                child: UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage('https://images.unsplash'
-                              '.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
-                          fit: BoxFit.cover)),
-                  currentAccountPicture: CircleAvatar(
-                    backgroundImage: NetworkImage(picture),
-                  ),
-                  accountName: Text('Allauddin'),
-                  accountEmail: Text('allauddin2015@gmail.com'),
+                height: 100,
+                width: 100,
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(picture),
                 ),
               ),
-              ListTile(
-                leading: Container(
-                  padding: EdgeInsets.all(6),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.red,
-                  ),
-                  child: Icon(
-                    Icons.home,
-                    size: 34,
-                    color: Colors.white,
-                  ),
-                ),
-                title: Text(
-                  'Home',
-                ),
-                subtitle: Text('Dashboard'),
+              SizedBox(
+                height: 20,
               ),
-              ListTile(
-                leading: Container(
-                  padding: EdgeInsets.all(6),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.red,
-                  ),
-                  child: Icon(
-                    Icons.home,
-                    size: 34,
-                    color: Colors.white,
-                  ),
+              Text(
+                (name != '') ? name : 'Home',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
                 ),
-                title: Text(
-                  'Second Page',
-                ),
-                subtitle: Text('Other Page'),
               ),
               Container(
-                decoration: BoxDecoration(
-                    border: Border(
-                  bottom: BorderSide(
-                      color: Colors.redAccent,
-                      width: 1,
-                      style: BorderStyle.solid),
-                )),
+                margin: EdgeInsets.only(left: 25, right: 25, top: 20),
+                color: Colors.white,
                 child: ListTile(
-                  leading: Container(
-                    padding: EdgeInsets.all(6),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
-                    ),
-                    child: Icon(
-                      Icons.home,
-                      size: 34,
-                      color: Colors.white,
-                    ),
-                  ),
-                  title: Text(
-                    'Home',
-                  ),
-                  subtitle: Text('Dashboard'),
+                  leading: Icon(Icons.email),
+                  title: Text('allauddin2015@gmail.com'),
                 ),
               ),
-              ListTile(
-                leading: Container(
-                  padding: EdgeInsets.all(6),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.red,
-                  ),
-                  child: Icon(
-                    Icons.home,
-                    size: 34,
-                    color: Colors.white,
-                  ),
+              Container(
+                margin: EdgeInsets.only(left: 25, right: 25, top: 20),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text('allauddin2015@gmail.com'),
                 ),
-                title: Text(
-                  'Home',
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 25, right: 25, top: 20),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text('allauddin2015@gmail.com'),
                 ),
-                subtitle: Text('Dashboard'),
               )
             ],
-          ),
-        ),
-        body: Container(
-            width: double.infinity,
-            color: Colors.deepPurpleAccent,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  height: 100,
-                  width: 100,
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(picture),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  (name != '') ? name : 'Home',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22.0,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 25, right: 25, top: 20),
-                  color: Colors.white,
-                  child: ListTile(
-                    leading: Icon(Icons.email),
-                    title: Text('allauddin2015@gmail.com'),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 25, right: 25, top: 20),
-                  color: Colors.white,
-                  child: ListTile(
-                    leading: Icon(Icons.email),
-                    title: Text('allauddin2015@gmail.com'),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 25, right: 25, top: 20),
-                  color: Colors.white,
-                  child: ListTile(
-                    leading: Icon(Icons.email),
-                    title: Text('allauddin2015@gmail.com'),
-                  ),
-                )
-              ],
-            )),
-      ),
+          )),
     );
   }
 }
